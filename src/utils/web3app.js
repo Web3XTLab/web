03cee6b934866368dc3ebcdc245bb4b16dbfa2aa
nikogu <644506165@qq.com>
2022-06-11 21:54:10 +0800

    fix: one ether test

--- src/utils/web3app.js
+++ src/utils/web3app.js
@@ -1,6 +1,8 @@
 import Web3 from "web3";
 import AppStoreAbi from "@/src/abis/AppStore.json";
 
+const oneEther = Web3.utils.toBN(10**18);
+
 const App = {
   web3Provider: null,
   contracts: {},
@@ -56,7 +58,7 @@ const App = {
       const account = accounts[0];
       console.log("account", account);
 
-      const amount = 1000000000;
+      const amount = oneEther;
       const tokenURI = `https://abcoathup.github.io/SampleERC1155/api/token/${Math.random()}.json`;
 
       App.contracts.AppStore.events
@@ -94,8 +96,8 @@ const App = {
         });
 
       await App.contracts.AppStore.methods
-        .buy(0)
-        .send({ from: account, value: 1000000000 });
+        .buy(1)
+        .send({ from: account, value: oneEther });
     } catch (e) {
       console.error(e);
     }
@@ -116,7 +118,7 @@ const App = {
           console.log(error);
         });
 
-      const result = await App.contracts.AppStore.methods.verify(0, account).call();
+      const result = await App.contracts.AppStore.methods.verify(1, account).call();
       console.log(result);
     } catch (e) {
       console.error(e);
