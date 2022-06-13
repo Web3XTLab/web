2cfd8f996db2826c5e2894b9d505b9cbf82064b8
Soulike <soulike@qq.com>
2022-06-13 20:44:59 +0800

    chore: modify the name and params of the API

--- packages/dapp-store-verify/src/index.ts
+++ packages/dapp-store-verify/src/index.ts
@@ -4,10 +4,9 @@ let initialized = false;
 
 /**
  * @param appTokenId - The token id of the app
- * @param consumerAddress - The address of the verified consumer 
  * @returns Whether the verified consumer has bought the app
  */
-export async function isAppBuyer(appTokenId: number, consumerAddress: number): Promise<boolean>
+export async function isAuthenticated(appTokenId: number): Promise<boolean>
 {
     if (!initialized)
     {
@@ -17,5 +16,5 @@ export async function isAppBuyer(appTokenId: number, consumerAddress: number): P
 
     // TODO: remove after the underlying API is stable
     // @ts-ignore
-    return web3app.verify(appTokenId, consumerAddress);
+    return web3app.verify(appTokenId);
 }
