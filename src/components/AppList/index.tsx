45806ac5b96ce1f60336fd4be16c0c346eb3ad76
nikogu <644506165@qq.com>
2022-06-15 13:38:20 +0800

    feat: optimize list data

--- src/components/AppList/index.tsx
+++ src/components/AppList/index.tsx
@@ -1,60 +1,71 @@
 import useWeb3 from "@/src/hooks/useWeb3";
 import { useEffect, useState } from "react";
 import styled from "styled-components";
-import axios from "axios";
-import Item from "./AppItem";
+import { Spinner } from "@fluentui/react/lib/Spinner";
+import Item, { AppItemDataType } from "./AppItem";
 
 type PropsType = {
-  onItemClick: (data: Record<string, any>) => void;
+  onItemClick: (data: AppItemDataType) => void;
 };
 
 const List = styled.div`
   display: flex;
   margin-top: 12px;
+  flex-wrap: wrap;
 `;
 
 const Title = styled.h4`
   margin-top: 32px;
 `;
 
+const NormalContent = styled.div`
+  padding: 24px 0;
+  font-size: 13px;
+  color: #605e5c;
+`;
+
 export default ({ onItemClick }: PropsType) => {
-  const [list, setList] = useState<any>([]);
+  const [listStatus, setListStatus] = useState("loading");
+  const [list, setList] = useState<string[]>([]);
   const web3 = useWeb3();
 
   useEffect(() => {
     (async () => {
-      const dataList = [];
       const list = await web3.tokenURIs();
-      if (!list) return;
-      for (let i = 0; i < list.length; i++) {
-        try {
-          const data = await axios.get(list[i], { timeout: 5000 });
-          dataList.push({
-            ...data.data,
-            // TODO: need be change
-            _tokenId: i,
-          });
-        } catch (e) {
-          console.error(e);
-          continue;
-        }
+      if (!list) {
+        setListStatus("empty");
+        return;
       }
-      setList(dataList);
+
+      // loading
+      setListStatus("loaded");
+      setList(list);
     })();
   }, []);
 
   return (
     <>
       <Title>Popular Apps</Title>
-      <List>
-        {list.map((item: any, id: number) => (
-          <Item
-            key={`${item.name}-${id}`}
-            data={item}
-            onItemClick={() => onItemClick(item)}
-          />
-        ))}
-      </List>
+      {listStatus === "loading" && (
+        <NormalContent>
+          <Spinner label="Seriously, still loading..." labelPosition="right" />
+        </NormalContent>
+      )}
+      {listStatus === "loaded" && (
+        <List>
+          {list.map((uri: string, id: number) => (
+            <Item
+              key={`${id}`}
+              data={{
+                tokenURI: uri,
+                _tokenId: id,
+              }}
+              onItemClick={onItemClick}
+            />
+          ))}
+        </List>
+      )}
+      {listStatus === "empty" && <NormalContent>No Content Here</NormalContent>}
     </>
   );
 };
